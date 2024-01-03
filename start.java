547eb66211b7d0051bcd6b77c1d185abd26dff02
Shaashvat Mittal <shaashvatmittal2004@gmail.com>
2024-01-03 01:11:34 -0700

    update on progress

--- start.java
+++ start.java
@@ -1,7 +1,7 @@
-public class start
-{
-    public static void main (String[] args)
-    {
-        System.out.println("Hello World");
+public class start {
+    public static void main(String[] args) {
+        System.out.println("Started with the video to develop the app");
+        System.out.println("Includes everything to make the app.");
+        System.out.println("Will Begin codeing now.");
     }
 }
